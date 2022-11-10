fa76ca20c595c45c5cbf234ad0c5c56feb8dfc0f
CristopherRamirezC <cristopherramirez506@gmail.com>
2022-11-09 22:48:09 -0600

    Proyecto1estructuradedatos

---
+++ Progra2proyecto clase/src/progra2proyecto/clase/Progra2proyectoClase.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progra2proyecto.clase;
+
+/**
+ *
+ * @author 50686
+ */
+public class Progra2proyectoClase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
